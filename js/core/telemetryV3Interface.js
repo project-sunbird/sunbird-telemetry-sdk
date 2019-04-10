6e73cbbc5534edbd0716fddfd3d1d71381871a29
raghu <http://localhost:9000>
2019-04-10 12:01:51 +0530

    Issue #SB-10785 fix : date sec to milli seconds changes

--- js/core/telemetryV3Interface.js
+++ js/core/telemetryV3Interface.js
@@ -402,8 +402,8 @@ var Telemetry = (function() {
      */
     instance.getEvent = function(eventId, data) {
         telemetryInstance.telemetryEnvelop.eid = eventId;
-        // timeDiff is diff of server date and local date 
-        telemetryInstance.telemetryEnvelop.ets = (new Date()).getTime() + (Telemetry.config.timeDiff || 0);
+        // timeDiff (in sec) is diff of server date and local date 
+        telemetryInstance.telemetryEnvelop.ets = (new Date()).getTime() + ((Telemetry.config.timeDiff*1000) || 0);
         telemetryInstance.telemetryEnvelop.ver = Telemetry._version;
         telemetryInstance.telemetryEnvelop.mid = '';
         telemetryInstance.telemetryEnvelop.actor = Object.assign({}, { "id": Telemetry.config.uid || 'anonymous', "type": 'User' }, instance.getUpdatedValue('actor'));
